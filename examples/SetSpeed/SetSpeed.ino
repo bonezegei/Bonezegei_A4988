30938953835b74fb24ed3f5a9ff21bd077f0a40a
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2024-03-07 20:51:08 +0800

    Create SetSpeed.ino

---
+++ examples/SetSpeed/SetSpeed.ino
@@ -0,0 +1,29 @@
+/*
+  This Library is written for A4988 Set Speed Example
+  Author: Bonezegei (Jofel Batutay)
+  Date: March 2024 
+*/
+
+#include <Bonezegei_A4988.h>
+
+#define FORWARD 1
+#define REVERSE 0
+
+#define PIN_DIR  8
+#define PIN_STEP 9
+
+Bonezegei_A4988 stepper(PIN_DIR, PIN_STEP);
+
+void setup(){
+  stepper.begin();
+  
+  //Speed microseconds per step
+  stepper.setSpeed(1000); 
+}
+
+void loop(){
+  stepper.step(FORWARD, 200);
+  delay(1000);
+  stepper.step(REVERSE, 200);
+  delay(1000);
+}
